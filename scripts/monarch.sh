a3cb580aca34a8c542b2f71e45c3408e1d53fed7
Monstroe <colemontrose@gmail.com>
2024-05-04 22:48:13 -0400

    Debug statements

--- scripts/monarch.sh
+++ scripts/monarch.sh
@@ -231,23 +231,32 @@ if [[ "${disk_device}" =~ "nvme" ]]; then
     disk_part3=${disk_device}p3
 fi
 
+echo "Disk device: $disk_device"
+echo "Partition 1: $disk_part1"
+echo "Partition 2: $disk_part2"
+echo "Partition 3: $disk_part3"
+echo "EFI size: $efi_size"
+echo "Swap size: $swap_size"
+echo
+echo "Clearing disk..."
+
 sgdisk --clear ${disk_device}
 # Check if UEFI is enabled
 if [ -d "/sys/firmware/efi" ]; then
     echo "UEFI system detected, partitioning disk..."
-    sgdisk --new 1::+${efi_size} --typecode=1:ef00 --change-name=1:"EFI System Partition" ${disk_device}
-    sgdisk --new 2::+${swap_size} --typecode=2:8200 --change-name=2:"Linux Swap" ${disk_device}
-    sgdisk --new 3::-0 --typecode=3:8300 --change-name=3:"Linux File System" ${disk_device}
+    sgdisk --new 1::+$efi_size --typecode=1:ef00 --change-name=1:"EFI System Partition" $disk_device
+    sgdisk --new 2::+$swap_size --typecode=2:8200 --change-name=2:"Linux Swap" $disk_device
+    sgdisk --new 3::-0 --typecode=3:8300 --change-name=3:"Linux File System" $disk_device
 
     echo "Formatting the partitions..."
-    mkfs.fat -F 32 ${disk_part1}
-    mkswap ${disk_part2}
-    mkfs.ext4 ${disk_part3}
+    mkfs.fat -F 32 $disk_part1
+    mkswap $disk_part2
+    mkfs.ext4 $disk_part3
 
     echo "Mounting file systems..."
-    mount --mkdir ${disk_part1} /mnt/boot
-    swapon ${disk_part2}
-    mount ${disk_part3} /mnt
+    mount --mkdir $disk_part1 /mnt/boot
+    swapon $disk_part2
+    mount $disk_part3 /mnt
 else
     echo "BIOS system detected, partitioning disk..."
     sgdisk --new 1::+${swap_size} --typecode=2:8200 --change-name=2:"Linux Swap" ${disk_device}
